d1ca0ff6fe03442788620c5f3d93c0034fe2c249
Harsh Talpada <neo.harsh@gmail.com>
2020-09-16 10:25:42 +1000

    Frameless window with resize and move

--- RoboKitty.py
+++ RoboKitty.py
@@ -1,21 +1,44 @@
-from PyQt5.QtWidgets import QTabWidget,QApplication
-from PyQt5.QtGui import QMouseEvent,QIntValidator,QKeyEvent,QCloseEvent,QResizeEvent
+from PyQt5.QtWidgets import QTabWidget,QApplication,QSizeGrip,QWidget,QVBoxLayout,QFrame,QHBoxLayout,QPushButton
+from PyQt5.QtGui import QMouseEvent,QKeyEvent,QCloseEvent,QResizeEvent
 import sys,pickle
 from utils.ConfigFile import Config
 from motionRecordUI import Ui_MainWindow
 from utils.AX12 import AX12A
+from PyQt5 import QtCore
 
 from uiHelper.CalibrateServoTab import CalibrateServoTab
 from uiHelper.SetupTab import SetupTab
 from uiHelper.ManualControlTab import ManualControlTab
 from uiHelper.MotionRecorderTab import MotionRecorderTab
+from pyqt5_material import apply_stylesheet
 
-
-class MainWindow(QTabWidget,Ui_MainWindow):
+class MainWindow(QWidget,Ui_MainWindow):
 
     def __init__(self,parent=None):
         super(MainWindow, self).__init__(parent)
         self.config = Config()
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.tabWidget = QTabWidget()
+        self._mainLayout = QVBoxLayout()
+        self._topBar = QHBoxLayout()
+        self.windowCloseButton = QPushButton()
+        self.windowCloseButton.setText("X")
+        self.windowCloseButton.setFixedWidth(25)
+        self.windowCloseButton.setFixedHeight(25)
+        self.windowCloseButton.clicked.connect(lambda :self.close())
+        self._windowMoveLine = QFrame()
+        self._windowMoveLine.setFrameShadow(QFrame.Plain)
+        self._windowMoveLine.setStyleSheet("background: black;")
+        self._windowMoveLine.setFrameShape(QFrame.HLine)
+        self._windowMoveLine.setMinimumSize(10,10)
+        self._topBar.addWidget(self._windowMoveLine)
+        self._topBar.addWidget((self.windowCloseButton))
+        self._mainLayout.addLayout(self._topBar)
+        self._mainLayout.addWidget(self.tabWidget)
+        self.setLayout(self._mainLayout)
+        self._windowMoveLine.mousePressEvent = self.windowBarMove
+        self._windowMoveLine.mouseMoveEvent = self.windowBarMove
+
         try:
             self.config = pickle.load(open('config.pkl','rb'))
 
@@ -42,13 +65,24 @@ class MainWindow(QTabWidget,Ui_MainWindow):
 
         self.ax12aInstances = []
         AX12A.debugEnabled = False
-        self.setupUi(self)
-        # self.setFixedSize(890,400)
-        self.setCurrentIndex(1)
+        sizeGrip = QSizeGrip(self)
+        sizeGrip.setVisible(True)
+        self._mainLayout.addWidget(sizeGrip, 0, QtCore.Qt.AlignBottom | QtCore.Qt.AlignRight)
+        self.setupUi(self.tabWidget)
+
+        self.tabWidget.setCurrentIndex(1)
         self.init()
 
     def keyPressEvent(self, a0: QKeyEvent) -> None:
         pass
+
+    def windowBarMove(self,event:QMouseEvent):
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            self.oldPos = event.globalPos()
+        elif event.type() == QtCore.QEvent.MouseMove:
+            delta = QtCore.QPoint(event.globalPos() - self.oldPos)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.oldPos = event.globalPos()
 
     # def resizeEvent(self, a0: QResizeEvent) -> None:
     #     QTabWidget.resizeEvent(self,a0)
@@ -101,7 +135,7 @@ class MainWindow(QTabWidget,Ui_MainWindow):
 
 
     def init(self):
-        self.currentChanged.connect(self.tabChanged)
+        self.tabWidget.currentChanged.connect(self.tabChanged)
         self.calibrateServoTab = CalibrateServoTab(self)
         self.setupTab = SetupTab(self)
         self.manualControlTab = ManualControlTab(self)
@@ -112,6 +146,8 @@ class MainWindow(QTabWidget,Ui_MainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     windows = MainWindow()
+    windows.move(app.primaryScreen().size().width()/2-600,app.primaryScreen().size().height()/2-400)
+    apply_stylesheet(app,theme='dark_teal.xml')
     windows.show()
     app.exec_()
 
